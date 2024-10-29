9315cc43def2987eb7ff36106640b867310b7732
sidra Tahir <sidratahir145@gmail.com>
2024-10-29 15:30:54 +0400

    added lessons js

---
+++ lessons.js
@@ -0,0 +1,82 @@
+let lessons=[ 
+    {
+      id: 1,
+      subject: "Math",
+      Location: "Dubai",
+      price: 2000,
+      image: "Assests/maths.jpeg",
+      availableInventory: 6
+    },
+    {
+      id: 2,
+      subject: "English",
+      Location: "Sharjah",
+      price: 1000,
+      image: "Assests/english.avif",
+      availableInventory: 9
+    },
+    {
+      id: 3,
+      subject: "Music",
+      Location: "Ajman",
+      price: 4980,
+      image: "Assests/music.jpg",
+      availableInventory: 4
+    },
+    {
+      id: 4,
+      subject: "Cricket",
+      Location: "Abu Dhabi",
+      price: 1950,
+      image: "Assests/cricket.png",
+      availableInventory: 10
+    },
+    {
+      id: 5,
+      subject: "biology",
+      Location: "Dubai",
+      price: 3900,
+      image: "Assests/biology.webp",
+      availableInventory: 5
+    },
+    {
+      id: 6,
+      subject: "chemistry",
+      Location: "Sharja",
+      price: 5600,
+      image: "Assests/chemistry.jpeg",
+      availableInventory: 7
+    },
+    {
+      id: 7,
+      subject: "physics",
+      Location: "Ajman",
+      price: 1800,
+      image: "Assests/physics.jpeg",
+      availableInventory: 8
+    },
+    {
+      id: 8,
+      subject: "Accountancy",
+      Location: "Abu dhabi",
+      price: 500,
+      image: "Assests/accountancy.jpeg",
+      availableInventory: 20
+    },
+    {
+      id: 9,
+      subject: "Computer",
+      Location: "ras al khaima",
+      price: 200,
+      image: "Assests/computer.jpeg",
+      availableInventory: 15
+    },
+    {
+      id: 10,
+      subject: "pshycology",
+      Location: "Ajman",
+      price: 1600,
+      image: "Assests/pshycology.jpeg",
+      availableInventory: 13
+    },
+  ]
